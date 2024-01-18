e8847b56b9a09ab22b76fb34d649ddcff38ff1e5
Nah-Nova <noaheutz@hotmail.nl>
2024-01-18 17:34:20 +0100

    chore: todo’s
    Signed-off-by: Nah-Nova <noaheutz@hotmail.nl>

---
+++ screens/EventAnalyticScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EventAnalyticScreen = () => {
+  return (
+	<View>
+		{/* TODO: add analytics for event */}
+	  <Text>EventAnalyticScreen</Text>
+	</View>
+  )
+}
+
+export default EventAnalyticScreen
