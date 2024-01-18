48714d9c41dfde715a33e0070dc392c5a8f54f4e
Nah-Nova <noaheutz@hotmail.nl>
2024-01-18 17:34:04 +0100

    feat: activity creation
    Signed-off-by: Nah-Nova <noaheutz@hotmail.nl>

--- screens/EventDetailScreen.js
+++ screens/EventDetailScreen.js
@@ -3,30 +3,178 @@ import {
   Text,
   StyleSheet,
   Image,
-  TextInput,
   TouchableOpacity,
-  ScrollView,
   SafeAreaView,
+  View,
+  FlatList,
 } from "react-native";
-import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
-import { useNavigation } from "@react-navigation/native";
-import * as Location from "expo-location";
-import MapView from "react-native-maps";
+import {
+  collection,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import useAuth from "../hooks/useAuth";
 import { db } from "../firebase";
+import ProfileHeader from "../components/ProfileHeader";
+import Header from "../components/Header";
+import { Plus } from "react-native-feather";
+import ListItem from "../components/ListItem";
 
 const EventDetailScreen = () => {
   const { userInfo, user } = useAuth();
   const navigation = useNavigation();
+  const { params } = useRoute();
+  const { location, event, } = params;
+
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    let unsub;
+    const fetchActivities = async () => {
+      try {
+        // checks if user has already passed a profile
+        unsub = onSnapshot(
+          query(
+            collection(
+              db,
+              "locations",
+              location.id,
+              "events",
+              event.id,
+              "activities"
+            )
+          ),
+          (snapshot) => {
+            setActivities(
+              snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+              }))
+            );
+          }
+        );
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    fetchActivities();
+    return unsub;
+  }, []);
+
   return (
     <>
       {user?.role === "organizer" ? (
         <SafeAreaView>
-          <Text>organizer event screen</Text>
+          <ProfileHeader />
+          <Header
+            title={`Welcome to ${event.title}`}
+            subtitle={`${event.description} at ${event.time}`}
+            hasBackButton={true}
+          />
+          <Image source={{ uri: event.photoUrl }} style={styles.image} />
+          <Header
+            title="Activities"
+            subtitle={
+              activities.length > 0
+                ? `This event has ${activities.length} ${
+                    activities.length === 1 ? "activity" : "activities"
+                  }`
+                : "There are no activities for this event add some!"
+            }
+            rightComponent={
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#fff",
+                  padding: 16,
+                  borderRadius: 4,
+                  marginHorizontal: 16,
+                }}
+                onPress={() =>
+                  navigation.navigate("ActivityManage", {
+                    location: location,
+                    event: event,
+                  })
+                }
+              >
+                <Plus
+                  color={"#000"}
+                  style={{
+                    width: 24,
+                    height: 24,
+                  }}
+                />
+              </TouchableOpacity>
+            }
+          />
+          <FlatList
+            data={activities}
+            renderItem={({ item }) => (
+              <>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity
+                    style={styles.activityButton}
+                    onPress={() =>
+                      navigation.navigate("ActivityManage", {
+                        location: location,
+                        event: event,
+                        activity: item,
+                      })
+                    }
+                  >
+                    <Text>Edit</Text>
+                  </TouchableOpacity>
+                </View>
+                <ListItem
+                  title={item.title}
+                  description={item.description}
+                  time={item.time}
+                  photoUrl={item.photoUrl}
+                />
+              </>
+            )}
+            keyExtractor={(item) => item.id + "organizing"}
+          />
         </SafeAreaView>
       ) : (
         <SafeAreaView>
-          <Text>attendee event screen</Text>
+          <ProfileHeader />
+          <Header
+            title={`Welcome to ${event.title}`}
+            subtitle={`${event.description} at ${event.time}`}
+            hasBackButton={true}
+          />
+          <Image source={{ uri: event.photoUrl }} style={styles.image} />
+          <Header
+            title="Activities"
+            subtitle={
+              activities.length > 0
+                ? `This event has ${activities.length} ${
+                    activities.length === 1 ? "activity" : "activities"
+                  }`
+                : "There are no activities for this event yet!"
+            }
+          />
+          <FlatList
+            data={activities}
+            renderItem={({ item }) => (
+              <ListItem
+                title={item.title}
+                description={item.description}
+                time={item.time}
+                photoUrl={item.photoUrl}
+                onPress={() =>
+                  navigation.navigate("ActivityDetail", {
+                    location: location,
+                    event: event,
+                    activity: item,
+                  })
+                }
+              />
+            )}
+            keyExtractor={(item) => item.id + "attending"}
+          />
         </SafeAreaView>
       )}
     </>
@@ -34,3 +182,33 @@ const EventDetailScreen = () => {
 };
 
 export default EventDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: "100%",
+    height: 100,
+    resizeMode: "cover",
+    marginTop: 16,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    marginTop: 16,
+    borderTopWidth: 1,
+    paddingVertical: 8,
+    borderTopColor: "#ccc",
+  },
+  activityButton: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 4,
+    marginHorizontal: 16,
+    alignItems: "center",
+  },
+});
---
+++ screens/EventManageScreen.js
@@ -0,0 +1,212 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+} from "react-native";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import useAuth from "../hooks/useAuth";
+import { db } from "../firebase";
+import Header from "../components/Header";
+
+// TODO: add query to save event to user's organizingEvents collection
+
+const EventManageScreen = () => {
+  const { userInfo } = useAuth();
+  const navigation = useNavigation();
+  const { params } = useRoute();
+  const { locationId, event } = params;
+
+  // event properties to be updated or created
+  const [title, setTitle] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [photoUrl, setPhotoUrl] = useState(null);
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
+
+  const incompleteEvent = !title || !description || !photoUrl || !date || !time;
+
+  const updateOrCreateEvent = () => {
+    // if event.id exists, update event else create new event
+    const eventsCollectionRef = collection(
+      db,
+      "locations",
+      locationId,
+      "events"
+    );
+
+    const eventPayload = {
+      title: title,
+      description: description,
+      photoUrl: photoUrl,
+      timestamp: serverTimestamp(),
+      organizer: userInfo.uid,
+      date: date,
+      time: time,
+      timestamp: serverTimestamp(),
+    };
+
+    if (event?.id) {
+      const eventDocRef = doc(eventsCollectionRef, event.id);
+      setDoc(eventDocRef, eventPayload)
+        .then(() => {
+          console.log("Document successfully written!");
+          Alert.alert(
+            "Event successfully updated! U may now close this modal 🎉"
+          );
+          navigation.goBack();
+        })
+        .catch((error) => {
+          console.error("Error writing document: ", error);
+        });
+    } else {
+      addDoc(eventsCollectionRef, eventPayload)
+        .then(() => {
+          console.log("Document successfully added!");
+          Alert.alert(
+            "Event successfully added! U may now close this modal 🎉"
+          );
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+    }
+  };
+
+  const getEvent = async () => {
+    const docRef = doc(db, "locations", locationId, "events", event?.id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { title, description, photoUrl, date, time } = docSnap.data();
+      setTitle(title);
+      setDescription(description);
+      setPhotoUrl(photoUrl);
+      setDate(date);
+      setTime(time);
+    } else {
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    getEvent();
+  }, [event]);
+
+  return (
+    <View style={styles.modalContainer}>
+      <Header title={event?.id ? "Update Event" : "Create Event"} />
+      <ScrollView
+        style={styles.formContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <Text style={styles.modalTitle}>Title</Text>
+        <TextInput
+          style={styles.modalInput}
+          placeholder="Enter the event title"
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Text style={styles.modalTitle}>Description</Text>
+        <TextInput
+          style={styles.modalInput}
+          placeholder="Enter the event description"
+          value={description}
+          onChangeText={setDescription}
+        />
+        <Text style={styles.modalTitle}>Photo URL</Text>
+        <TextInput
+          style={styles.modalInput}
+          placeholder="Enter the event photo URL"
+          value={photoUrl}
+          onChangeText={setPhotoUrl}
+        />
+        <Text style={styles.modalTitle}>Date</Text>
+        <TextInput
+          style={styles.modalInput}
+          placeholder="Enter the event date"
+          value={date}
+          onChangeText={setDate}
+        />
+        <Text style={styles.modalTitle}>Time</Text>
+        <TextInput
+          style={styles.modalInput}
+          placeholder="Enter the event time"
+          value={time}
+          onChangeText={setTime}
+        />
+      </ScrollView>
+      <TouchableOpacity
+        style={[styles.modalButton, { opacity: incompleteEvent ? 0.3 : 1 }]}
+        onPress={updateOrCreateEvent}
+        disabled={incompleteEvent}
+      >
+        <Text style={styles.modalButtonText}>
+          {event?.id ? "Update Event" : "Create Event"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default EventManageScreen;
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 5,
+    paddingHorizontal: 16,
+  },
+  modalText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    padding: 4,
+    textAlign: "center",
+  },
+  formContainer: {
+    alignSelf: "stretch",
+  },
+  modalTitle: {
+    textAlign: "left",
+    paddingVertical: 8,
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#000",
+  },
+  modalInput: {
+    fontSize: 20,
+    borderWidth: 1,
+    borderColor: "#D8D8D8",
+    borderRadius: 16,
+    padding: 16,
+    marginVertical: 8,
+    width: "100%",
+  },
+  modalButton: {
+    width: "100%",
+    padding: 16,
+    marginBottom: 24,
+    borderRadius: 16,
+    backgroundColor: "#000",
+    position: "absolute",
+    bottom: 0,
+  },
+  modalButtonText: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+});
